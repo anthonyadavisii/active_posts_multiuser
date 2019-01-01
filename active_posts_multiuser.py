c1ceb1836cfe5c008be54f18f367e00c16d62c13
Anthony A Davis <v7davisa@yahoo.com>
2019-01-01 13:29:54 -0600

    Update active_posts_multiuser.py
    tab to spaces

--- active_posts_multiuser.py
+++ active_posts_multiuser.py
@@ -38,7 +38,7 @@ def active_posts_multiuser_bl_check(userlist):
         gen = a.blog_history()
         for b in gen:
             if b.is_pending():
-			    c = Comment(b.identifier)
+                c = Comment(b.identifier)
                 post_dict = {
                                 'Author': u,
                                 'Comment': '[Comment](https://steemit.com/'+b.identifier+')', 
